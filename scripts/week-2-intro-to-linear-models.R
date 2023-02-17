62072b3f3167f4be088dc6161c1c6d039f86859a
Jasmine Westover <geq21adu@uea.ac.uk>
2023-02-17 12:54:34 +0000

    Committed to prevent any loss of script- continuing to work through the session to get a better understanding of how linear models work in R.

--- scripts/week-2-intro-to-linear-models.R
+++ scripts/week-2-intro-to-linear-models.R
@@ -4,10 +4,14 @@ library(GGally)
 library(emmeans)
 library(performance)
 
+# _______________________________ ####
+
 ## 13.1 A linear model analysis for comparing groups ----
 # general function for fitting linear models, part of base R
 # "least-squares model 0" example being used below, the linear-model uses a techniques called least-squares
 lsmodel0 <- lm(formula = height ~ 1, data = darwin)
+
+# _____________________________ ####
 
 # 13.2 Summaries for Models ----
 # investigate a summary of the model using summary function (seen below)
@@ -19,7 +23,6 @@ broom::tidy(lsmodel0)  # summarize info about model components
 broom::glance(lsmodel0) # reports info about the entire model
 broom::augment(lsmodel0) # adds info about individual observations to a dataset and it can be used to model
                          # predictions onto a new dataset
-
 ## 13.2.2 Model Summary ----
 # Can use either basic R function or a tidyverse function
 # Can use the basic R summary function as seen above
@@ -30,4 +33,34 @@ broom::augment(lsmodel0) # adds info about individual observations to a dataset 
 mean(darwin$height)  # answer = 18.88333 (rounded = 18.9)
 
 ## 13.2.3 Compare Means ----
+# we want a linear model that can analyse the differences in average plant height, can use the lm() function
+lsmodel1 <- lm(height ~ type, data=darwin)
+
+# note that the following is identical
+# lsmodel1 <- lm(height ~ 1 + type, data=darwin)  the model formula contains the pollination type in addition to an intercept
+
+broom::tidy(lsmodel1) # the second row in the tibble, shows the difference in the mean height of he 2 groups
+
+# This linear model indicates the average height of Crossed plants is 20.2 inches.
+# Selfed plants are an average of 2.6 inches shorter, we are able to confirm this using the formula below:
+
+darwin %>% 
+  group_by(type) %>% 
+  summarise(mean=mean(height))
+
+# we can use the information provided by the model apply the calculated means onto a plot
+darwin %>% 
+  ggplot(aes(x=type, 
+             y=height,
+             colour=type))+
+  geom_jitter(alpha=0.5,
+              width=0.1)+
+  stat_summary(fun=mean,
+               size=1.2)+
+  theme_bw()
+
+## 13.2.4 Standard error of the difference -----
+# LM output provides the standard errors of the values.
+# The first row calculates mean & standard error of the mean(SEM).
+# As the second row provides the mean difference- the standard error of the difference between the two means (SED) is provided, which can also be calculated using a formula.
 
